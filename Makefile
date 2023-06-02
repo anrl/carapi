71a2c8a6c2687065a3601c13bed805e4d33010d4
anotherLostKitten <eggismagicchickensquared@gmail.com>
2023-06-02 10:20:13 -0400

    mode to header file only

--- Makefile
+++
@@ -1,9 +0,0 @@
-all: car_api.o
-	ar rcs  car_api.a *.o
-	rm *.o
-
-car_api.o: car_api.c car_api.h
-	gcc -c car_api.c -lwiringPi
-
-clean:
-	rm *.o *.a;
--- car_api.h
+++ car_api.h
@@ -5,7 +5,6 @@
 #include <wiringPi.h>
 // this makes use of the wiringPi interface for GPIO control
 // found at http://wiringpi.com/download-and-install/
-// you must run your c program by linking to the object car_api.o file and compile with -lwiringPi
 
 #if  defined __has_include
 #if __has_include ("pins.h")
@@ -29,20 +28,56 @@ struct Car{
 
 	float turning_radius;//radius the car requires to turn
 	float angular_velocity;//degrees the car turns per unit time
-
 };
 
-struct Car setup_default_car();//setup a car with PINS_DEFAULT values
+struct Car setup_new_car(int lp0,int lp1,int rp0,int rp1){//specify which pins correspond to which side of the car
+	struct Car new_car;
+	new_car.left_pin_0=lp0;
+	new_car.left_pin_1=lp1;
+	new_car.right_pin_0=rp0;
+	new_car.right_pin_1=rp1;
+	wiringPiSetup();// sets up the wiring pi library
+	pinMode(lp0,OUTPUT);//setup each pin to output mode
+	pinMode(lp1,OUTPUT);
+	pinMode(rp0,OUTPUT);
+	pinMode(rp1,OUTPUT);
+	return new_car;
+}
 
-struct Car setup_new_car(int lp0,int lp1,int rp0,int rp1);//create a new car and specify corresponding WIRINGPi Pin numbers (NOTE THAT THIS IS DIFFERENT FROM BOTH BCM AND PIN NUMBERS).
-//WIRING PI PIN NUMBERS ARE FOUND HERE: https://pinout.xyz/ Hover over the corresponding pin with the mouse to find the WIRING PI Numbers
+struct Car setup_default_car(){
+    return setup_new_car(PINS_DEFAULT[0], PINS_DEFAULT[1], PINS_DEFAULT[2], PINS_DEFAULT[3]);
+}
 
-void move_car_fwd(struct Car car);// move car forward constantly
+void move_car_fwd(struct Car car){//moves the car forward at max motor speed
+	digitalWrite(car.left_pin_0,HIGH);
+	digitalWrite(car.left_pin_1,LOW);
+	digitalWrite(car.right_pin_0,HIGH);
+	digitalWrite(car.right_pin_1,LOW);
+}
+void move_car_bwd(struct Car car){//moves the car backward
+	digitalWrite(car.left_pin_0,LOW);
+	digitalWrite(car.left_pin_1,HIGH);
+	digitalWrite(car.right_pin_0,LOW);
+	digitalWrite(car.right_pin_1,HIGH);
+}
 
-void move_car_bwd(struct Car car);// move car backwards constantly
+void stop_car(struct Car car){//stop the vehicle
+	digitalWrite(car.left_pin_0,LOW);
+	digitalWrite(car.left_pin_1,LOW);
+	digitalWrite(car.right_pin_0,LOW);
+	digitalWrite(car.right_pin_1,LOW);
+}
 
-void move_car_left(struct Car car);// turn car left constantly
+void move_car_left(struct Car car){//moves the car left
+	digitalWrite(car.left_pin_0,LOW);
+	digitalWrite(car.left_pin_1,LOW);
+	digitalWrite(car.right_pin_0,HIGH);
+	digitalWrite(car.right_pin_1,LOW);
+}
 
-void move_car_right(struct Car car);// turn car right constantly
-
-void stop_car(struct Car car);// stop car
+void move_car_right(struct Car car){// moves the car right
+	digitalWrite(car.left_pin_0,HIGH);
+	digitalWrite(car.left_pin_1,LOW);
+	digitalWrite(car.right_pin_0,LOW);
+	digitalWrite(car.right_pin_1,LOW);
+}
